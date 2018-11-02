c167491d71f60b408862f1b72832e24d608fbf43
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-11-01 20:05:36 -0700

    added Added documentation

--- Deep.py
+++ Deep.py
@@ -6,21 +6,23 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+#show a sample
 cv2.imshow("%s" %y_test[0], x_test[0])
 cv2.waitKey(0)
 
 model = tf.keras.models.Sequential([
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(512, activation=tf.nn.relu),
-      tf.keras.layers.Dropout(0.2),
-      tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+      tf.keras.layers.Flatten(), #Take in an image, and vectorize it
+      tf.keras.layers.Dense(512, activation=tf.nn.relu), #add a densely connected layer with exactly 512 neurons
+      tf.keras.layers.Dropout(0.2), #Add a "dropout" layer, to combat overfitting
+      tf.keras.layers.Dense(10, activation=tf.nn.softmax) #Create an output layer
 ])
+
 model.compile(optimizer='adam', 
-loss='sparse_categorical_crossentropy',
-metrics=['accuracy'])
-#model.predict(x_test)
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5)
-#print(sum(model.predict(np.ndarray.flatten(x_test[i]))==y_test[i] for i in range(len(x_test)))/len(x_test))
+
 loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
+
 print("accuracy: " + str(accuracy))
-#model.predict(x_test, y_test)
